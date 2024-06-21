34f4b1864fbf1ef006d01d3b9dc2645708b3178d
SouthernMinami <kanou373@gmail.com>
2024-06-22 06:30:10 +0900

    Add: consoleファイル追加、imagesテーブルのマイグレーション

---
+++ Database/Migrations/2024-06-21_1718981917_CreateImagesTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreateImagesTable implements SchemaMigration
+{
+    public function up(): array
+    {
+        // マイグレーション処理を書く
+        return [
+            'CREATE TABLE images (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(50),
+                image_path VARCHAR(255),    
+                post_url VARCHAR(255),
+                delete_url VARCHAR(255),
+                view_count INT,
+                ip_address VARCHAR(255),
+                file_size double,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                accessed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )'
+        ];
+    }
+
+    public function down(): array
+    {
+        // ロールバック処理を書く
+        return [];
+    }
+}
