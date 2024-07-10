51f958bcfb46b474b44fc433a16b12a97fb48788
SouthernMinami <kanou373@gmail.com>
2024-07-10 14:44:20 +0900

    Feat: 大体のサイトレイアウト

--- Views/home.php
+++ Views/home.php
@@ -6,5 +6,15 @@ namespace Views;
 
 <div class="title-container">
     <h1 class="page-title">Welcome to NORATTER</h1>
-    <p class="page-description"></p>
+    <h2 class="page-description">
+        Share images of cute animals!
+    </h2>
+    <div class="sample">
+        <h3>Let's upload photos of Wild Animals...</h3>
+        <img src="/public/imgs/wild_cat.png" alt="wild cat" class="sample-img">
+    </div>
+    <div class="sample">
+        <h3>or of the daily life of Your Pets!</h3>
+        <img src="/public/imgs/pet_dog.png" alt="pet dog" class="sample-img">
+    </div>
 </div>
---
+++ Views/layout/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+
+    <title>NORATTER</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul className="navbar flex items-center w-full h-full space-x-4">
+                <li class="nav-item text-4xl font-bold text-white pb-2 pl-3 pr-2">
+                    <h1><a href="/" class="logo">NORATTER</a></h1>
+                </li>
+                <div class="links">
+                    <li class="nav-item nav-link flex items-center justify-between text-slate-400">
+                        <p><a href="/new" class="post-btn">NEW POST</a></p>
+                    </li>
+                    </li>
+                    <li class="nav-item nav-link flex items-center justify-between text-slate-400">
+                        <p><a href="/all_images" class="images-btn">ALL IMAGES</a></p>
+                    </li>
+                </div>
+
+            </ul>
+        </nav>
+    </header>
+    <main class="container mt-5 mb-5"></main>
