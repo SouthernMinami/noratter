1c60e1c12ae57585f2de64f759a44a31d657d2b0
SouthernMinami <kanou373@gmail.com>
2024-07-29 08:40:36 +0900

    Fix: カラム名のtypoを修正

--- Views/image.php
+++ Views/image.php
@@ -18,7 +18,7 @@ DatabaseHelper::updateImage($image['post_path']);
 <div class="d-flex flex-column p-4 m-3 w-50 border border-secondary rounded post-area">        
     <h3>Title: <?php echo htmlspecialchars($image['title'], ENT_QUOTES, 'UTF-8'); ?></h2>
     <div id="preview" class="pb-3 preview">
-        <img id="preview-img" src="<?php echo htmlspecialchars($image['imagePath'], ENT_QUOTES, 'UTF-8'); ?>" alt="preview image">
+        <img id="preview-img" src="<?php echo htmlspecialchars($image['image_path'], ENT_QUOTES, 'UTF-8'); ?>" alt="preview image">
     </div>    
     <p>Description: <?php echo htmlspecialchars($image['description'], ENT_QUOTES, 'UTF-8'); ?></p>
     <p>Views: <?php echo htmlspecialchars($image['view_count'], ENT_QUOTES, 'UTF-8'); ?></p>
