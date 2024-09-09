7d2482203122bba6a31e12579ab35c26ac4f4316
SouthernMinami <kanou373@gmail.com>
2024-09-09 23:13:05 +0900

    Add: faviconを追加

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../public/imgs/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="../public/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
